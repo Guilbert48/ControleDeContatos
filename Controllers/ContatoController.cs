0bdbf9a3f194ada3bf6b9c9d6bcebaff9286bb2c
guilbert silva <guilberts48@gmail.com>
2024-03-04 10:30:42 -0300

    Atualizações de front end

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -8,5 +8,19 @@ namespace ControleDeContatos.Controllers
         {
             return View();
         }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
     }
 }
